d40e607109515ae883438f3a6ddb184faf89581c
solivarafaela <rafaelasoliva@outlook.com>
2022-11-25 00:16:39 -0300

    Funções definidas em uma outra classe

--- java/src/calculadora/main.java
+++ java/src/calculadora/main.java
@@ -1,22 +1,54 @@
+package calculadora;
+
 import java.util.Scanner;
 
-public class Calculadora {
+public class main {
     public static void main(String[] args) {
 
         char operador;
         float operandoUm, operandoDois, resultado = 0;
-        int numeroFuncao;
         boolean isOperadorValido = true;
-        boolean isExecutandoPrincipal = true;
-        boolean isExecutandoSecundario = true;
+        boolean isExecutando = true;
 
         Scanner scanner = new Scanner(System.in);
+        Calculadora calculadora = new Calculadora();
 
-        System.out.println("Escolha uma função:");
-        System.out.println("1 - Calculadora Simples");
-        System.out.println("2 - Raiz quadrada:");
-        System.out.println("3 - Tabuada");
+        System.out.println("Insira o primeiro número:");
+        operandoUm = scanner.nextFloat();
+        System.out.println("Insira o operador (+ - * /): ");
+        operador = scanner.next().charAt(0);
+        System.out.println("Insira o segundo número:");
+        operandoDois = scanner.nextFloat();
 
-        numeroFuncao = scanner.nextInt();
+        switch (operador) {
+            case '+': System.out.println(calculadora.somar(operandoUm, operandoDois)); break;
+            case '-': System.out.println(calculadora.subtrair(operandoUm, operandoDois)); break;
+            case '*': System.out.println(calculadora.multiplicar(operandoUm, operandoDois)); break;
+            case '/': System.out.println(calculadora.dividir(operandoUm, operandoDois)); break;
+            default: System.out.println("Operador inválido!");
+        }
+
+        do {
+            System.out.println("Insira o primeiro número (01 para interromper):");
+            operandoUm = scanner.nextFloat();
+
+            if (operandoUm == 01) {
+                System.out.println("\nPrograma encerrado!");
+                isExecutando = false;
+            } else {
+                System.out.println("Insira o operador (+ - * /): ");
+                operador = scanner.next().charAt(0);
+                System.out.println("Insira o segundo número:");
+                operandoDois = scanner.nextFloat();
+
+                switch (operador) {
+                    case '+': System.out.println(calculadora.somar(operandoUm, operandoDois)); break;
+                    case '-': System.out.println(calculadora.subtrair(operandoUm, operandoDois)); break;
+                    case '*': System.out.println(calculadora.multiplicar(operandoUm, operandoDois)); break;
+                    case '/': System.out.println(calculadora.dividir(operandoUm, operandoDois)); break;
+                    default: System.out.println("Operador inválido!");
+                }
+            }
+        } while (isExecutando);
     }
 }
