d422fc86eeb356ae8cc90612f34c410d3e63beb8
solivarafaela <rafaelasoliva@outlook.com>
2022-11-24 22:54:25 -0300

    Código agora roda até ser interrompido pelo usuário

--- java/src/CalculadoraSimples.java
+++ java/src/CalculadoraSimples.java
@@ -6,6 +6,7 @@ public class CalculadoraSimples {
         float operandoUm, operandoDois, resultado = 0;
         char operador;
         boolean isOperadorValido = true;
+        boolean isExecutandoValido = true;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -27,5 +28,32 @@ public class CalculadoraSimples {
         if (isOperadorValido) {
             System.out.println(resultado);
         }
+
+        do {
+            System.out.println("Insira o primeiro número (01 para interromper):");
+            operandoUm = scanner.nextFloat();
+
+
+            if (operandoUm == 01) {
+                isExecutandoValido = false;
+            } else {
+                System.out.println("Insira o operador (+ - * /): ");
+                operador = scanner.next().charAt(0);
+                System.out.println("Insira o segundo número:");
+                operandoDois = scanner.nextFloat();
+
+                switch (operador) {
+                    case '+' -> resultado = operandoUm + operandoDois;
+                    case '-' -> resultado = operandoUm - operandoDois;
+                    case '*' -> resultado = operandoUm * operandoDois;
+                    case '/' -> resultado = operandoUm / operandoDois;
+                    default -> isOperadorValido = false;
+                }
+
+                if (isOperadorValido) {
+                    System.out.println(resultado);
+                }
+            }
+        } while (isExecutandoValido);
     }
 }
