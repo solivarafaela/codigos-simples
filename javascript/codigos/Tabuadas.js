d7993cc21ec00603b7e900a3cbcd4946f7038f13
solivarafaela <rafaelasoliva@outlook.com>
2022-12-08 21:34:30 -0300

    Código separado em funções

--- javascript/codigos/Tabuadas.js
+++ javascript/codigos/Tabuadas.js
@@ -1,11 +1,16 @@
 var prompt = require('prompt-sync');
 var prompt = prompt();
 
+
+function retornarTabuada(numeroLimite, numero) {
+    for (i = 0; i <= numeroLimite; i++) {
+        console.log(numero + ' x ' + i + ' = ' + (numero * i))
+    }
+}
+
 var numero, numeroLimite;
 
 numero = prompt('Informe um número para exibir sua tabuada: ');
 numeroLimite = prompt('Até que valor o número incial deve ser multiplicado? ')
 
-for (i = 0; i <= numeroLimite; i++) {
-    console.log(numero + ' x ' + i + ' = ' + (numero * i))
-}
+retornarTabuada(numeroLimite, numero);
