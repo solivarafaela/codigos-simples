749a69eea6d157ab1a0f5c34ce51a2c21949ec90
solivarafaela <rafaelasoliva@outlook.com>
2022-12-09 12:36:17 -0300

    Código separado em funções

--- javascript/codigos/Pares e ímpares.js
+++ javascript/codigos/Pares e ímpares.js
@@ -1,14 +1,19 @@
+//Recebe um número e o classifica em par ou ímpar.
+
 var prompt = require('prompt-sync');
 var prompt = prompt();
 
+
+function classificarNumero(numero) {
+    if (numero === 0) {
+        return 'Esse número é par.';
+    } else {
+        return 'Esse número é impar.';
+    }
+}
+
 var numero;
-
-numero = prompt('Digite um número: ')
-
+numero = prompt('Digite um número: ');
 numero = numero % 2;
 
-if (numero === 0) {
-    console.log('Esse número é par.');
-} else {
-    console.log('Esse número é impar.');
-}
+console.log(classificarNumero(numero));
