988381dc1689c09912d25565994811207db80fb4
solivarafaela <rafaelasoliva@outlook.com>
2022-12-09 15:23:40 -0300

    Código separado em funções e saída melhorada

--- javascript/codigos/Viagem viável.js
+++ javascript/codigos/Viagem viável.js
@@ -3,22 +3,41 @@
 var prompt = require('prompt-sync');
 var prompt = prompt();
 
-var consumoMedioCombustivel, quantidadeAtualCombustivel, distanciaViagem, valorCombustivel;
-var combustivelNecessario, deficitCombustivel, valorNecessarioParaViajar;
+function calcularCombustivelNecessario(distanciaViagem, consumoMedioCombustivel) { //Retorna a quantidade de combustível necessário.
+    return distanciaViagem / consumoMedioCombustivel;
+}
 
-consumoMedioCombustivel = parseFloat(prompt('Informe o consumo médio de combustível do veículo (Km por litro): '));
-quantidadeAtualCombustivel = parseFloat(prompt('Informe quanto combustível há no tanque (Litros): '));
+function calcularDeficitCombustivel(quantidadeAtualCombustivel, combustivelNecessario) { //Retorna quanto combustível falta/resta.
+    return quantidadeAtualCombustivel - combustivelNecessario;
+}
+
+function calcularCustoAbastecimento(deficitCombustivel, valorCombustivel) { //Retorna o custo do abastecimento necessário.
+    return (deficitCombustivel * (-1)) * valorCombustivel;
+}
+
+var distanciaViagem, quantidadeAtualCombustivel, consumoMedioCombustivel, valorCombustivel; //Variáveis de entrada.
+var combustivelNecessario, deficitCombustivel, custoAbastecimento; //Variáveis de processamento.
+
 distanciaViagem = parseFloat(prompt('Informe a distância da viagem (Km): '));
+quantidadeAtualCombustivel = parseFloat(prompt('Informe quanto combustível há no tanque (Litros): '));
+consumoMedioCombustivel = parseFloat(prompt('Informe o consumo médio de combustível do veículo (Km por litro): '));
 
-combustivelNecessario = distanciaViagem / consumoMedioCombustivel;
-deficitCombustivel = quantidadeAtualCombustivel - combustivelNecessario;
+combustivelNecessario = calcularCombustivelNecessario(distanciaViagem, consumoMedioCombustivel);
+deficitCombustivel = calcularDeficitCombustivel(quantidadeAtualCombustivel, combustivelNecessario);
 
-if (deficitCombustivel >= 0) {
+//Seleção de saída:
+if (deficitCombustivel > 0) {
+    deficitCombustivel * (-1);
+
     console.log('\nAo final da viagem você terá aproximadamente: ' + deficitCombustivel + ' litro(s) restantes.');
     console.log('Boa viagem!');
+} else if (deficitCombustivel == 0) {
+    console.log('\nAtenção! Viagem pode ser realizada SEM reserva de combustível.');
 } else {
     valorCombustivel = parseFloat(prompt('Informe o valor do litro do combustível: '));
-    valorNecessarioParaViajar = deficitCombustivel * valorCombustivel;
-    console.log('\nVocê precisa abastecer ' + (deficitCombustivel * (-1)) + ' litro(s) de combustível.'); //invertendo valor negativo.
-    console.log('O valor estimado é de: ' + (valorNecessarioParaViajar * (-1)) + ' reais.'); //invertendo valor negativo.
+    custoAbastecimento = calcularCustoAbastecimento(deficitCombustivel, valorCombustivel);
+    deficitCombustivel = deficitCombustivel * (-1);
+
+    console.log('\nVocê precisa abastecer ' + (deficitCombustivel) + ' litro(s).');
+    console.log('O custo da viagem será de: ' + custoAbastecimento + ' real(is).')
 }
